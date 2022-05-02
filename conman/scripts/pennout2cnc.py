4a83d6c2fd2da59bc485d53e97b53ec29fab83a7
rainsfordtm <tmr740-ac@yahoo.co.uk>
2022-05-02 17:28:52 +0200

    Worked on penn out conversion script.

--- conman/scripts/pennout2cnc.py
+++ conman/scripts/pennout2cnc.py
@@ -30,6 +30,21 @@ def script(transformer, tree, keyword_attr = '', keyword_node_regex = ''):
         m = re.search(r'/\*([^/]+)\*/\s*$', full_comment)
         if not m: return ''
         return m.group(1).strip()
+        
+    def get_head(ic):
+        nonlocal tree
+        is_ip = True if re.match('IP.*', ic.getAttribute('cat')) else False
+        xjs = tree.find_child_nodes('cat', '.J', regex=True)
+        if is_ip and xjs:
+            return tree.get_child_leaves(xjs[0])[0]
+        else:
+            return tree.get_child_leaves(tree.get_child_branches(ic)[0])[0]
+        
+    def is_head(leaf, ic):
+        nonlocal tree
+        # Returns True if leaf is the head of this constituent.
+        # Convoluted expression finds all descendent leaves
+        return True if leaf is get_head(ic) else False  
     
     ################################
     # 1. Deal with the comment node
@@ -87,9 +102,23 @@ def script(transformer, tree, keyword_attr = '', keyword_node_regex = ''):
         l.reverse()
         leaf.setAttribute('ancestors', '|'.join(l))
             
+    ###################################################################
+    # 6. Do some very head identification to preserve structure for CoNLL
+    # Uses the 'order' attribute, so will fail if leaves are removed
+    # Head of an IP is the first leaf whose tag is .J, head of anything
+    # else is the first word in the constituent.
+    ###################################################################
     
-    
-    
+    tree.add_leaf_attr('conll_HEAD')    
+    for leaf in tree.leaves:
+        ic = leaf.parentNode.parentNode
+        while is_head(leaf, ic) and ic.parentNode is not tree.trunk:
+            ic = ic.parentNode
+        if is_head(leaf, ic):
+            # Head is sentence root
+            leaf.setAttribute('conll_HEAD', '0')
+        else:
+            leaf.setAttribute('conll_HEAD', str(get_head(ic).getAttribute('order')))
     
     return tree
     
