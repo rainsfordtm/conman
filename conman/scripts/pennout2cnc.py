a0bef5b3728481d7d3cb1a84d51f3b8ed5abba40
rainsfordtm <tmr740-ac@yahoo.co.uk>
2022-05-02 16:50:49 +0200

    Work on Penn-out to cnc script.

--- conman/scripts/pennout2cnc.py
+++ conman/scripts/pennout2cnc.py
@@ -23,25 +23,46 @@ def script(transformer, tree, keyword_attr = '', keyword_node_regex = ''):
         comment_branches = tree.find_nodes('comment', '.+', regex=True)
         if not comment_branches: return ''
         full_comment = comment_branches[0].getAttribute('comment')
+        # NOW REMOVE COMMENT ATTRIBUTE
+        comment_branches[0].removeAttribute('comment')
+        tree._refresh_lists()
+        # DONE
         m = re.search(r'/\*([^/]+)\*/\s*$', full_comment)
         if not m: return ''
         return m.group(1).strip()
     
+    ################################
     # 1. Deal with the comment node
+    ################################
     # Get the comment
     comment = get_keynode_comment()
     # Find keyword node number using keyword_node_regex
-    print(comment)
     m = re.match(keyword_node_regex, comment)
     keyword_node = m.groupdict()['keyword_node']
-    print(keyword_node)
-    print(type(keyword_node))
     # Add the keyword attr to all branches
     tree.add_branch_attr(keyword_attr)
     # Find the keyword in the tree
     keyword_branch = tree.find_nodes('cs_id', keyword_node, regex=False)[0]
     # Set the attribute to True
     keyword_branch.setAttribute(keyword_attr, 'True')
+    
+    ###############################
+    # 2. Remove all code nodes
+    ###############################
+    nodes = tree.find_nodes('cat', 'CODE', regex=False)
+    while nodes:
+        tree.del_node_deep(nodes.pop(0))
+    
+    ####################################################################
+    # 3. Deal with the reference node
+    # It's the final leaf in the tree
+    # In order to save it for posterity, it is stored as transformer.ref
+    # Then the node is deleted.
+    #####################################################################
+    tree.sort()
+    node = tree.leaves[-1]
+    transformer.ref = node.getAttribute('value')
+    tree.del_node_deep(node.parentNode)
     
     return tree
     
