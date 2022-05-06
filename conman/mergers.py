84706de924d9fddcc6d16addbab60ec7073e97f6
rainsfordtm <tmr740-ac@yahoo.co.uk>
2022-05-06 15:52:05 +0200

    Added a concordance merger (untested)

--- conman/mergers.py
+++ conman/mergers.py
@@ -3,9 +3,8 @@
 class ConcordanceMerger():
     """
     Class used to merge two concordance.Concordance objects. Matching
-    hits are identified using hit.uuid > hit.ref > order of hits in this 
-    order of priority; one or more of these may be enabled or disabled by
-    setting the use_uuid, use_ref and use_order parameters of the concordance.
+    hits are identified using hit.uuid, hit.ref or order of hits and this
+    may be specified using the uuid, ref and order parameters of the concordance.
     The default settings modify the concordance minimally by updating the
     hit.tags dictionary only.
     
@@ -16,23 +15,23 @@ class ConcordanceMerger():
         is False.
     del_hits (bool):
         Delete hits from the concordance which aren't found in the merging
-        concordance. Default is False.
+        concordance. Uses UUIDs and therefore only applies where use_uuid is
+        set to True. Default is False.
+    match_by (str):
+        A string telling the merger how to match hits. The only possible values
+        are 'uuid' and 'ref'. If empty string (default), assumes list index.
     update_tags (bool):
         Update values already present in hit.tags with new values from the
         merging concordance. Default is True.
-    use_uuid (bool):
-        Use hit.uuid to identify identical hits. Default is True.
-    use_ref (bool):
-        Use hit.ref to identify identical hits. Default is True.
-    use_order (bool):
-        Use the order of the hits in the concordance to identify identical
-        rows. Default is True.
     token_merger (mergers.TokenMerger) :
         Provides a TokenMerger to add token-level data from other_cnc to cnc.
         If None provided, tokens are left unchanged. Default is None.
     
     Methods:
     --------
+    match_hit(self, cnc, other_hit):
+        Finds the hit in cnc which corresponds to other_hit. Returns
+        None if nothing can be found.
     merge(self, cnc, other_cnc):
         Modifies the concordance cnc by adding data from concordance
         other_cnc.
@@ -44,9 +43,43 @@ class ConcordanceMerger():
         """
         self.add_hits, self.del_hits = False, False
         self.update_tags = True
-        self.use_uuid, self.use_ref, self.use_order = True, True, True
+        self.match_by = ''
         self.token_merger = None
         
+    def match_hit(self, cnc, other_hit, ix):
+        """
+        Finds the hit in cnc which corresponds to other_hit. Returns
+        None if nothing can be found.
+        
+        Parameters:
+            cnc (concordance.Concordance):      The concordance to be modified
+            other_hit (concordance.Hit):        The hit to be matched.
+            ix (int):                           The index of other_hit.
+            
+        Returns:
+            match_hit(self, cnc, other_hit):
+                The corresponding hit in self.cnc, or None if not found.
+        """
+        if self.match_by == 'uuid':
+            l = cnc.get_uuids()
+            return cnc[l.index(other_hit.uuid)] if other_hit.uuid in l else None
+        l = [(hit.ref, i) for i, hit in enumerate(cnc)]
+        if self.match_by = 'ref':
+            matches = list(filter(lambda x: x[0] == other_hit.ref, l))
+            if len(matches) == 0:
+                # No matching reference
+                return None
+            if len(matches) == 1:
+                # One matching reference
+                return cnc[matches[0][1]]
+            if len(matches) > 1:
+                # More than one matching reference; reset l so it only contains 
+                # matching references
+                l = matches
+        # Assume match by list index
+        matches = list(filter(lambda x: x[1] == ix, l))
+        return cnc[matches[0][1]] if matches else None
+
     def merge(self, cnc, other_cnc):
         """
         Modifies the concordance cnc by adding data from concordance
@@ -60,5 +93,28 @@ class ConcordanceMerger():
             merge(self, cnc, other_cnc):
                 A modified cnc concordance.
         """
+        for ix, other_hit in enumerate(other_cnc):
+            hit = self.match_hit(cnc, other_hit, ix)
+            if not hit:
+                if self.add_hits:
+                    cnc.append(other_hit)
+                continue
+            if update_tags:
+                hit.tags.update(other_hit.tags)
+            else:
+                # i.e. only add new tags, so take a copy and perform 2 updates.
+                d = hit.tags.copy()
+                hit.tags.update(other_hit.tags)
+                hit.tags.update(d)
+            # Call the token_merger, if one is given.
+            if self.token_merger:
+                self.token_merger.merge(hit, other_hit)
+        # MUST check use_uuid because if the cncs use different UUIDs it will
+        # delete every hit in the first cnc.
+        if self.del_hits and self.match_by == 'uuid':
+            l = other_cnc.get_uuids()
+            cnc = Concordance(list(
+                filter(lambda x: x.uuid in l, cnc)
+            ))
         return cnc
         
